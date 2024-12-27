35dae2a8980f3a92676ff194684b188296564d64
Charles <charlesabryanjr@gmail.com>
2024-12-27 07:08:55 -0600

    122724

--- README.md
+++
@@ -1,1 +0,0 @@
-# coding_interview_questions
--- rodCutting.py
+++
@@ -1,7 +0,0 @@
-
-def rodCutting(arr, n):
-    revenue = [0]
-    for i in range(len(arr)):
-        for j in range(i):
-            revenue[i] = revenue[j] + rodCutting(arr, i - j)
-    return revenue[-1]
