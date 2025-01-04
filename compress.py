91f98d41e66796fb7b8218a0107e5d92435daba7
Charles <charlesabryanjr@gmail.com>
2025-01-04 15:27:52 -0600

    010425

---
+++ compress.py
@@ -0,0 +1,97 @@
+# pylint: skip-file
+
+'''
+443. String Compression
+Given an array of characters chars, compress it using the following algorithm:
+
+Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+If the group's length is 1, append the character to s.
+Otherwise, append the character followed by the group's length.
+The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+After you are done modifying the input array, return the new length of the array.
+
+You must write an algorithm that uses only constant extra space.
+
+Example 1:
+
+Input: chars = ["a","a","b","b","c","c","c"]
+Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+Example 2:
+
+Input: chars = ["a"]
+Output: Return 1, and the first character of the input array should be: ["a"]
+Explanation: The only group is "a", which remains uncompressed since it's a single character.
+Example 3:
+
+Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+
+Constraints:
+
+1 <= chars.length <= 2000
+chars[i] is a lowercase English letter, uppercase English letter, digit, or symbol.
+'''
+
+from typing import List
+
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if len(chars) == 0:
+            return 0
+
+        last_char_index = 0
+        last_char = chars[last_char_index]
+        last_char_count_index = last_char_index + 1
+        last_char_count = 1
+
+        for i in range(1, len(chars)):
+            if chars[i] == last_char:
+                last_char_count += 1
+            else:
+                chars[last_char_index] = last_char
+                if last_char_count == 1:
+                    last_char_index = i
+                    last_char = chars[i]
+                    last_char_count_index = last_char_index + 1
+                elif last_char_count > 1 and last_char_count <= 9:
+                    chars[last_char_index] = last_char
+                    chars[last_char_count_index] = str(last_char_count)
+
+                    last_char_index = last_char_count_index + 1
+                    last_char_count_index = last_char_index + 1
+                else:
+                    chars[last_char_index] = last_char
+                    chars[last_char_count_index] = str(last_char_count)
+
+                    last_char_index = last_char_count_index + 1
+                    last_char_count_index = last_char_index + 1
+
+                last_char_count = 1
+                last_char = chars[i]
+
+        if last_char_count == 1: 
+            pass
+        elif last_char_count > 1 and last_char_count <= 9: 
+            chars[last_char_index] = last_char
+            chars[last_char_count_index] = str(last_char_count)
+            chars = chars[:last_char_count_index+1]
+
+        print(f'chars: {chars}')
+        return len(chars)
+
+
+if __name__ == "__main__":
+    chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+    print(f'chars: {chars}')
+    solution = Solution()
+    OUTPUT = solution.compress(chars)
+    print(f'compress: {OUTPUT}')
+    # ["a","b","1","2"]
+    EXPECTED_OUTPUT = 4
+    print(f'Expected Output: {EXPECTED_OUTPUT}')
+    print(f'OUTPUT == EXPECTED_OUTPUT: {OUTPUT == EXPECTED_OUTPUT}')
