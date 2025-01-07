300ce9f458d4eb6885864ce76bab3e7dd5fe0e9e
Charles <charlesabryanjr@gmail.com>
2025-01-06 22:02:18 -0600

    010624

---
+++ isSubsequence.py
@@ -0,0 +1,52 @@
+# pylint: skip-file
+'''
+392. Is Subsequence
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+ 
+
+Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+'''
+
+from typing import List
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return True if i >= len(s) else False
+
+
+if __name__ == "__main__":
+    s = "abc"
+    t = "ahbgdc"
+    print(f's: {s}')
+    print(f't: {t}')
+    solution = Solution()
+    OUTPUT = solution.isSubsequence(s, t)
+    print(f'isSubsequence: {OUTPUT}')
+    EXPECTED_OUTPUT = True
+    print(f'Expected Output: {OUTPUT == EXPECTED_OUTPUT}')
